28cd6a414a5bfef502a179d49d9fcd094a119845
Anderson Alves <anderson_sa@outlook.com>
2020-08-09 21:45:12 +0200

    adding readme documentation

--- src/main/java/com/doodle/pollservice/controller/PollController.java
+++ src/main/java/com/doodle/pollservice/controller/PollController.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 import static java.lang.String.format;
